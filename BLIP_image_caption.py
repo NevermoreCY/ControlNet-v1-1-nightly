6f6525739202a50d9a0deabc8542c4cf90d5287d
nev <cy823236858@gmail.com>
2023-09-11 03:31:07 +0800

    developing

--- BLIP_image_caption.py
+++ BLIP_image_caption.py
@@ -65,7 +65,7 @@ def load_image(image_size, device, im_path):
     raw_image2 = read_image(im_path).to(device)
     load_4 = time.time()
     print("load_diff3", load_4 - load_3)
-    print(raw_image.shape , raw_image2.shape)
+    print(raw_image.shape , raw_image2[:3].shape)
 
     # print("raw_image shape", raw_image.size)
     # print("raw_image type", type(raw_image))
@@ -75,7 +75,7 @@ def load_image(image_size, device, im_path):
     load_5 = time.time()
     print("load_diff4", load_5 - load_4)
 
-    print(raw_image == raw_image2)
+    print(raw_image == raw_image2[:3])
     return image
 
 def load_image_batch(image_size, device, batch_path):
