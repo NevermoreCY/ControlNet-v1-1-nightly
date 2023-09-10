7f8dde03c9a13c993ec246b72e2e1441fd4ed065
nev <cy823236858@gmail.com>
2023-09-11 02:37:34 +0800

    developing

--- BLIP_image_caption.py
+++ BLIP_image_caption.py
@@ -114,12 +114,12 @@ print("total_n", total_n)  # 772870
 
 
 
-job_num = 13
+job_num = 14
 job_length = total_n // 8
 
 
-start_n = 26500
-end_n = 30000
+start_n = 33000
+end_n = 36500
 bz = 100
 
 print("******** cur job_num is " , job_num, "start is", start_n, "end is", end_n )
