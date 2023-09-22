61caa93bf247f2f3564a59099607cd2b35813f8e
nev <cy823236858@gmail.com>
2023-09-22 19:32:43 +0800

    developing

--- download_from_hugging.py
+++ download_from_hugging.py
@@ -1,3 +1,4 @@
 from huggingface_hub import hf_hub_download
 
-hf_hub_download(repo_id="lllyasviel/ControlNet", filename="control_sd15_canny.pth")
+x = hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_canny.pth")
+print(x)
