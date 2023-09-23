634f05d79036d1291aa90ce3270ab861da2e8d39
nev <cy823236858@gmail.com>
2023-09-24 01:55:47 +0800

    developing

--- cldm/logger.py
+++ cldm/logger.py
@@ -8,7 +8,7 @@ from pytorch_lightning.callbacks import Callback
 from pytorch_lightning.utilities.distributed import rank_zero_only
 
 
-class ImageLogger(Callback):
+class ImageLogger2(Callback):
     def __init__(self, batch_frequency=2000, max_images=4, clamp=True, increase_log_steps=True,
                  rescale=True, disabled=False, log_on_batch_idx=False, log_first_step=False,
                  log_images_kwargs=None, log_dir="logs/default"):
@@ -27,7 +27,7 @@ class ImageLogger(Callback):
 
     @rank_zero_only
     def log_local(self, save_dir, split, images, global_step, current_epoch, batch_idx):
-        root = os.path.join(save_dir, "image_log", split)
+        # root = os.path.join(save_dir, "image_log", split)
         root = os.path.join(self.log_dir)
         for k in images:
             grid = torchvision.utils.make_grid(images[k], nrow=4)
