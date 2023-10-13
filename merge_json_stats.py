5f3bf5c924ce443fb23b13d918e72c13124b9d03
nev <cy823236858@gmail.com>
2023-10-14 02:48:02 +0800

    developing

---
+++ merge_json_stats.py
@@ -0,0 +1,21 @@
+import json
+
+total_jobs= 38
+
+count_dict= {}
+for i in range(14):
+    count_dict[i] = []
+
+for i in range(total_jobs):
+    file_name = "BLIP_v2_data_split_" + str(i) + ".json"
+    with open(file_name, 'r') as f:
+        cur_json = json.load(f)
+    for key in cur_json:
+        count_dict[key].extend( cur_json[key] )
+
+for i in range(14):
+    print("for count i, we have ", len( count_dict[i]) , ' samples ')
+
+with open("BLIP2_split_by_count.json", 'w') as f:
+    json.dump(count_dict, f)
+
