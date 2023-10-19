3549ae06ba8db70a438c2ba166af7789d707a8b9
nev <cy823236858@gmail.com>
2023-10-19 18:57:11 +0800

    developing

--- test3.py
+++ test3.py
@@ -5,5 +5,5 @@ for i in range(5,14):
     file_pre = 'BLIP2_split_by_count_and_grayscale' + str(i) + '.json'
     with open(file_pre, 'r') as f:
         data = json.load(f)
-
+    print(data.keys())
     print('For count ', str(i) , 'grayscale ', len(data['grayscale']) , 'color count', len(data['color']))
