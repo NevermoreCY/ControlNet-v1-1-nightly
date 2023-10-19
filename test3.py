92c759d55551b944332fb48a726d0129e6d2cdd0
nev <cy823236858@gmail.com>
2023-10-19 18:56:41 +0800

    developing

--- test3.py
+++ test3.py
@@ -1,9 +1,5 @@
 import json
-import cv2
-import numpy as np
-from PIL import Image, ImageStat
-from tqdm import tqdm
-import os
+
 
 for i in range(5,14):
     file_pre = 'BLIP2_split_by_count_and_grayscale' + str(i) + '.json'
