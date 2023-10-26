82b8b2dac47fdfc7036c50ef0961e5830a6e7f66
nev <cy823236858@gmail.com>
2023-10-26 15:07:15 +0800

    developing

--- data_checking.py
+++ data_checking.py
@@ -17,40 +17,45 @@ def doArgs(argList):
 
 def main():
     args = doArgs(sys.argv[1:])
-    job_num = args.job_num
-    tag = 'cat'
-
-
-    imgs_folder = "/yuch_ws/views_release"
-
-    with open('BLIP2_split_by_count_V4.json') as f:
-        data_dict = json.load(f)
-
-    print("processing data for count ", job_num)
-    cur_data = data_dict[str(job_num)]
-    cur_out_dir = 'data_checking/' + tag + '/' + str(job_num)
-    os.makedirs(cur_out_dir,exist_ok=True)
-    data_list = []
-
     count = 0
-    for i in tqdm(range(len(cur_data))):
-        folder = cur_data[i]
-        img_folder = imgs_folder + "/" + folder
-        text_path = img_folder + "/BLIP_best_text_v2.txt"
-        with open(text_path,'r') as f:
-            line = f.readline()
-        words = line.split()
-        if tag in words or (tag + 's') in words:
-            count +=1
-            data_list.append(folder)
-            for idx in [0,3,7,11]:
-                im_path = os.path.join(img_folder , '%03d.png' % idx)
-                target_path = cur_out_dir + '/' + folder + ('%03d.png' % idx)
-                shutil.copy(im_path,target_path)
-            print(count, line)
+    for job_num in range(13):
+        # job_num = args.job_num
+        tag = 'cat'
 
-    with open('data_checking/' + tag + '/' + str(job_num) +'.json') as f :
-        json.dump(data_list,f)
+
+        imgs_folder = "/yuch_ws/views_release"
+
+        with open('BLIP2_split_by_count_V4.json') as f:
+            data_dict = json.load(f)
+
+        print("processing data for count ", job_num)
+        cur_data = data_dict[str(job_num)]
+        cur_out_dir = 'data_checking/' + tag + '/' + str(job_num)
+        os.makedirs(cur_out_dir,exist_ok=True)
+        data_list = []
+
+
+
+        for i in tqdm(range(len(cur_data))):
+            folder = cur_data[i]
+            img_folder = imgs_folder + "/" + folder
+            text_path = img_folder + "/BLIP_best_text_v2.txt"
+            with open(text_path,'r') as f:
+                line = f.readline()
+            words = line.split()
+            if tag in words or (tag + 's') in words:
+                count +=1
+                data_list.append(folder)
+                for idx in [0,3,7,11]:
+                    im_path = os.path.join(img_folder , '%03d.png' % idx)
+                    target_path = cur_out_dir + '/' + folder + ('%03d.png' % idx)
+                    shutil.copy(im_path,target_path)
+                # print(count, line)
+            if count % 100 ==0:
+                print(count)
+
+        with open('data_checking/' + tag + '/' + str(job_num) +'.json') as f :
+            json.dump(data_list,f)
 
 
 if __name__ == '__main__':
