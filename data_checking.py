1905ae51a20e82ba1e5ca729c4065b4af6ceaa72
nev <cy823236858@gmail.com>
2023-10-26 15:35:45 +0800

    developing

--- data_checking.py
+++ data_checking.py
@@ -18,7 +18,7 @@ def doArgs(argList):
 def main():
     args = doArgs(sys.argv[1:])
     count = 0
-    for job_num in range(6,13):
+    for job_num in range(0,6):
         # job_num = args.job_num
         tag = 'cat'
 
