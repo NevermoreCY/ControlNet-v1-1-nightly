a0367cab8210be2a3e01a9f4202134bb7fa9ca91
nev <cy823236858@gmail.com>
2023-10-26 14:52:17 +0800

    developing

---
+++ data_checking.py
@@ -0,0 +1,78 @@
+import os
+
+from PIL import Image
+import requests
+import torch
+from torchvision import transforms
+from torchvision.transforms.functional import InterpolationMode
+from tqdm import tqdm
+import json
+import time
+import numpy as np
+import sys
+from torchvision.io import read_image
+import argparse
+
+import objaverse
+from sentence_transformers import SentenceTransformer, util
+import shutil
+import nltk
+from nltk.tokenize import word_tokenize
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
+
+def doArgs(argList):
+    parser = argparse.ArgumentParser()
+
+    #parser.add_argument('-v', "--verbose", action="store_true", help="Enable verbose debugging", default=False)
+    parser.add_argument('--job_num',type=int, help="Input file name", required=True)
+    # parser.add_argument('--output', action="store", dest="outputFn", type=str, help="Output file name", required=True)
+
+    return parser.parse_args(argList)
+
+
+def main():
+    args = doArgs(sys.argv[1:])
+    job_num = args.job_num
+    tag = 'cat'
+
+
+    imgs_folder = "/yuch_ws/views_release"
+
+    with open('BLIP2_split_by_count_V4.json') as f:
+        data_dict = json.load(f)
+
+    print("processing data for count ", job_num)
+    cur_data = data_dict[str(job_num)]
+    cur_out_dir = 'data_checking/' + tag + '/' + str(job_num)
+    os.makedirs(cur_out_dir)
+    data_list = []
+
+    count = 0
+    for folder in cur_data:
+        img_folder = imgs_folder + "/" + folder
+        text_path = img_folder + "/BLIP_best_text_v2.txt"
+        with open(text_path,'r') as f:
+            line = f.readline()
+        words = line.split()
+        if tag in words or (tag + 's') in words:
+            count +=1
+            data_list.append(folder)
+            for idx in [0,3,7,11]:
+                im_path = os.path.join(img_folder , '%03d.png' % idx)
+                target_path = cur_out_dir + '/' + folder + ('%03d.png' % idx)
+                shutil.copy(im_path,target_path)
+            print(count, line)
+
+    with open('data_checking/' + tag + '/' + str(job_num) +'.json') as f :
+        json.dump(data_list,f)
+
+
+if __name__ == '__main__':
+    #sys.argv = ["programName.py","--input","test.txt","--output","tmp/test.txt"]
+    main()
+
+
+
+
+
