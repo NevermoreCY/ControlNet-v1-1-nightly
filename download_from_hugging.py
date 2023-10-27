8291319e37ca14dd6f0ef1f0934d528cf5ac4dc9
nev <cy823236858@gmail.com>
2023-10-27 16:23:48 +0800

    developing

--- download_from_hugging.py
+++ download_from_hugging.py
@@ -6,8 +6,8 @@ from huggingface_hub import login
 login()
 
 
-x = hf_hub_download(repo_id="ShapeNet/ShapeNetCore", filename="04554684.zip")
-print(x)
+# x = hf_hub_download(repo_id="ShapeNet/ShapeNetCore", filename="04554684.zip")
+# print(x)
 
 
 x = snapshot_download(repo_id="ShapeNet/shapenetcore-glb", repo_type='dataset')
