a326bf14f6bd73a6302e12f25fea6575a838b400
nev <cy823236858@gmail.com>
2023-10-27 16:20:20 +0800

    developing

--- download_from_hugging.py
+++ download_from_hugging.py
@@ -6,9 +6,9 @@ from huggingface_hub import login
 login()
 
 
-# x = hf_hub_download(repo_id="runwayml/stable-diffusion-v1-5", filename="v1-5-pruned.ckpt")
-# print(x)
+x = hf_hub_download(repo_id="ShapeNet/ShapeNetCore", filename="04554684.zip")
+print(x)
 
 
-x = snapshot_download(repo_id="ShapeNet/shapenetcore-glb")
+x = snapshot_download(repo_id="ShapeNet/shapenetcore-glb", repo_type='dataset')
 print(x)
