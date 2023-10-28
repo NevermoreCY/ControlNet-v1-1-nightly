4cc9ef418a0d465b9a78919223e344b6d29823d4
nev <cy823236858@gmail.com>
2023-10-28 21:08:54 +0800

    developing

--- download_from_hugging.py
+++ download_from_hugging.py
@@ -11,5 +11,6 @@ login()
 
 
 # x = snapshot_download(repo_id="ShapeNet/shapenetcore-glb", repo_type='dataset', cache_dir='/root/shapenet')
-x = snapshot_download(repo_id="ShapeNet/shapenetcore-glb", repo_type='dataset', cache_dir='./')
+x = snapshot_download(repo_id="ShapeNet/shapenetcore-glb", repo_type='dataset', local_dir="shape_data",local_dir_use_symlinks=False)
+
 print(x)
