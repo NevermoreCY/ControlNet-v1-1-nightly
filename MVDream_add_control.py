7fd1644c42720b51b1743e51ca949d2d5d09fcad
nev <cy823236858@gmail.com>
2024-02-18 18:07:38 +0800

    initial debugging

--- MVDream_add_control.py
+++ MVDream_add_control.py
@@ -1,5 +1,3 @@
-import sys
-import os
 
 # assert len(sys.argv) == 3, 'Args are wrong.'
 
@@ -13,7 +11,7 @@ output_path = 'base_models/mvcontrol_base.pt'
 # assert os.path.exists(os.path.dirname(output_path)), 'Output path is not valid.'
 
 import torch
-from share import *
+# from share import *
 from cldm.model import create_model
 
 
