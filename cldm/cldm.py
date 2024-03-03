faf3bb5f59299aa12a30fd025dd3411dd6b4bd92
nev <cy823236858@gmail.com>
2024-03-03 21:19:48 +0800

    initial debugging

--- cldm/cldm.py
+++ cldm/cldm.py
@@ -806,6 +806,8 @@ class ControlLDM(LatentDiffusion):
         # get camera info
         T = batch['camera_pose'].to(memory_format=torch.contiguous_format).float()
 
+        DEBUG=True
+
         if DEBUG:
             print("\n\n\n Before rearrange: control shape is ", control.shape )  # torch.Size([160, 3, 256, 256])
             print("\n shape of T is ", T.shape)     # torch.Size([40,4, 3, 4])
