202a8e1ca866533057d8dfe85c0666138f1f8309
nev <cy823236858@gmail.com>
2024-03-07 22:41:45 +0800

    initial debugging

--- cldm/cldm.py
+++ cldm/cldm.py
@@ -1228,6 +1228,10 @@ class ControlLDM(LatentDiffusion):
         n_row = min(z.shape[0], n_row)
         log["reconstruction"] = self.decode_first_stage(z)
         log["control"] = c_cat * 2.0 - 1.0
+
+
+        # batch_text = batch[self.cond_stage_key]
+
         log["conditioning"] = log_txt_as_img((512, 512), batch[self.cond_stage_key], size=16)
 
         if plot_diffusion_rows:
