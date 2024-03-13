1a1689af6dd35f251032e29b3b1153904b1491b5
nev <cy823236858@gmail.com>
2024-03-13 14:42:25 +0800

    initial debugging

--- check_params.py
+++ check_params.py
@@ -1,49 +1,146 @@
 
-# assert len(sys.argv) == 3, 'Args are wrong.'
-
-
-m_path = 'base_models/mvcontrol_base_v2.pt'
-m_path = '/yuch_ws/TripoSR/model.ckpt'
-
-# assert os.path.exists(input_path_control), 'Input model does not exist.'
-# assert not os.path.exists(output_path), 'Output filename already exists.'
-# assert os.path.exists(os.path.dirname(output_path)), 'Output path is not valid.'
+trained_weight_path = 'base_models/sd-v1.5-4view.pt'
+pretrained_weight_path = 'base_models/mvcontrol_base_v4.pt'
 
 import torch
 # from share import *
 from cldm.model import create_model
 
 
-
-# new model (MVDream + control)
-# model = create_model(config_path='./base_models/control_3D_sd15.yaml')
-# # model = create_model(config_path='./models/yuch_v11p_sd15_canny_full.yaml')
-#
-# print("model creation done!")
+def get_node_name(name, parent_name):
+    if len(name) <= len(parent_name):
+        return False, ''
+    p = name[:len(parent_name)]
+    if p != parent_name:
+        return False, ''
+    return True, name[len(parent_name):]
 
 # load pretrained A
-pretrained_weights_control = torch.load(m_path)
+pretrained_weights_control = torch.load(pretrained_weight_path)
 
 if 'state_dict' in pretrained_weights_control:
     pretrained_weights_control = pretrained_weights_control['state_dict']
 
 
-# load pretrained B
+# load trained B
 
-control_key = list(pretrained_weights_control.keys())
+pretrained_weights = torch.load(trained_weight_path)
 
-# print('\n\n\n\n\n',pretrained_weights_control.keys())
-# print('\n\n\n\n\n',pretrained_weights_control2.keys())
-# print('\n\n\n\n\n',pretrained_weights_mvd.keys())
+if 'state_dict' in pretrained_weights:
+    pretrained_weights = pretrained_weights['state_dict']
+
+# load c
+
+trained_weights = torch.load(trained_weight_path)
+
+if 'state_dict' in trained_weights:
+    trained_weights = trained_weights['state_dict']
 
 
-# print('\n\n\n\n\n\n  in control key 2, not in control key')
-# for item in control_key2:
+pretrained_key = list(pretrained_weights.keys())
+trained_key = list(trained_weights.keys()) # note all item in control_key2 are also included in control_key
+
+
+for item in pretrained_key:
+
+    pre_item = pretrained_weights[item]
+    trained_item = trained_weights[item]
+
+    same_item = pre_item == trained_item
+    x = torch.sum(same_item)
+
+    print(item , pre_item.shape , x)
+
+    # print(item, pretrained_weights_mvd[item].shape)
+#
+# print('\n\n\n\n\n\n  in control3d, not in mvd')
+# for item in control3D_key:
+#     if item not in mvd_key:
+#         print(item, control3D_dict[item].shape)
+#
+# print('\n\n\n\n\n\n  in control3d, not in control')
+# for item in control3D_key:
 #     if item not in control_key:
-#         print(item) # None
+#         print(item, control3D_dict[item].shape )
+#
+# print('\n\n\n\n\n\n  in mvd, not in con3d')
+# for item in mvd_key:
+#     if item not in control3D_key:
+#         print(item, pretrained_weights_mvd[item].shape)
+#
+#
+# print('\n\n\n\n\n\n  in con, not in con3d')
+# for item in control_key:
+#     if item not in control3D_key:
+#         print(item, pretrained_weights_control[item].shape)
+#
+#
+#
+#
+# target_dict = {}
+# # 0th step copy original weights, these are all the keys we nedd
+# for k in control3D_dict.keys():
+#     target_dict[k] = control3D_dict[k].clone()
+# # First copy control net v1.0 parameters
+# for k in pretrained_weights_control.keys():
+#     target_dict[k] = pretrained_weights_control[k].clone()
+# # second copy control net v1.1 parameters
+# for k in pretrained_weights_control2.keys():
+#     target_dict[k] = pretrained_weights_control2[k].clone()
+#
+# # copy mvd
+# for k in pretrained_weights_mvd.keys():
+#
+#     if ('model.diffusion_model.time_embed.' in k):
+#         print("time in MVD!, copy it")
+#         prefix_l = len('model.diffusion_model.time_embed.')
+#         sufix = k[prefix_l:]
+#         print('sufix:', sufix)
+#         target_pre = 'control_model.time_embed.'
+#         target_key = target_pre + sufix
+#         print("TO : " , target_key)
+#         target_dict[target_key] = pretrained_weights_mvd[k].clone()
+#     elif ('model.diffusion_model.camera_embed.' in k):
+#         print("camera in MVD!, copy it")
+#         prefix_l = len('model.diffusion_model.camera_embed.')
+#         sufix = k[prefix_l:]
+#         print('sufix:', sufix)
+#         target_pre = 'control_model.camera_embed.'
+#         target_key = target_pre + sufix
+#         print("TO : ", target_key)
+#         target_dict[target_key] = pretrained_weights_mvd[k].clone()
+#     elif ('model.diffusion_model.input_blocks.' in k):
+#         print(" copy input block from ", k)
+#         prefix_l = len('model.diffusion_model.input_blocks.')
+#         sufix = k[prefix_l:]
+#         print('sufix:', sufix)
+#         target_pre = 'control_model.input_blocks.'
+#         target_key = target_pre + sufix
+#         print("TO : ", target_key)
+#         target_dict[target_key] = pretrained_weights_mvd[k].clone()
+#     elif ('model.diffusion_model.middle_blocks.' in k):
+#         print("copy middle block from ", k)
+#         prefix_l = len('model.diffusion_model.middle_blocks.')
+#         sufix = k[prefix_l:]
+#         print('sufix:', sufix)
+#         target_pre = 'control_model.middle_blocks.'
+#         target_key = target_pre + sufix
+#         print("TO : ", target_key)
+#         target_dict[target_key] = pretrained_weights_mvd[k].clone()
+#     else:
+#         target_dict[k] = pretrained_weights_mvd[k].clone()
+#
+# # for k in pretrained_weights_mvd.keys():
+#
+#
+# to_discard = ["model.diffusion_model.time_embed.0.weight", "model.diffusion_model.time_embed.0.bias", "model.diffusion_model.time_embed.2.weight", "model.diffusion_model.time_embed.2.bias"]
+# for k in to_discard:
+#     target_dict.pop(k,None)
+#
+#
+# model.load_state_dict(target_dict, strict=True)
+# torch.save(model.state_dict(), output_path)
+# print('Done.')
 
-print('\n\n\n\n\n\n  in mvd, not in con3d')
-for item in control_key:
-    print(item, pretrained_weights_control[item].shape)
 
-
+#
